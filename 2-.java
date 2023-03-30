17ad1c6d73983c92c6e60d1355949a506b58bc1d
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-03-30 19:29:33 -0300

    Update and rename 2.java to 2-.java

--- 2-.java
+++ 2-.java
@@ -1,4 +1,4 @@
-public class III {
+public class 2- {
     public static void main (String [] args) {
         
        String z = "Bink 182;Twenty one pilots;Paramore;My chemical romance;Bob Marley;Charly Garcia;Nirvana;Queen;Lana del Rey;Pink Floyd;";
