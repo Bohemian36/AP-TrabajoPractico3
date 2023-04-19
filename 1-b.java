b7a2094f9cf5a4b846f6dac2cea1800d1867dc24
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-04-19 11:18:25 -0300

    Update 1-b.java

--- 1-b.java
+++ 1-b.java
@@ -1,36 +1,26 @@
 public class 1-b {
     public static void main (String [] args) {
      
-     
     int [] z = new int [3];
-    
     z [0] =27;
     z [1] =90;
     z [2] =36;
     
     System.out.println ("Lo números elegidos son: " + z[0]+"-"+z[1]+"-"+z[2]);
     System.out.println ("Y su decreciente es: ");
-    
-    
+       
     if (((z[0]>z[1]) && (z[0]>z[2])) && ((z[1]<z[0]) && (z[1]>z[2]))) {
         System.out.print(z[0]+" "+z[1]+" "+z[2]);
-        
-     } else if ((z[0]>z[1] && z[0]>z[2]) && (z[2]<z[0] && z[2]>z[1])) {
+    } else if ((z[0]>z[1] && z[0]>z[2]) && (z[2]<z[0] && z[2]>z[1])) {
          System.out.print(z[0]+" "+z[2]+" "+z[1]);
-     
-     } else if ((z[1]>z[0] && z[1]>z[2]) && (z[0]<z[1] && z[0]>z[2])) {
+    } else if ((z[1]>z[0] && z[1]>z[2]) && (z[0]<z[1] && z[0]>z[2])) {
           System.out.print(z[1]+" "+z[0]+" "+z[2]);
-       
-     } else if ((z[1]>z[0] && z[1]>z[2]) && (z[2]<z[1] && z[2]>z[0])) {
+    } else if ((z[1]>z[0] && z[1]>z[2]) && (z[2]<z[1] && z[2]>z[0])) {
          System.out.print(z[1]+" "+z[2]+" "+z[0]);
-     
-     } else if ((z[2]>z[0] && z[2]>z[1]) && (z[1]<z[2] && z[1]>z[0])) {
+    } else if ((z[2]>z[0] && z[2]>z[1]) && (z[1]<z[2] && z[1]>z[0])) {
          System.out.print(z[2]+" "+z[1]+" "+z[0]);
-     
-     }else{
-        
-     System.out.print(z[2]+" "+z[0]+" "+z[1]);
-       }
-       
+    }else{
+         System.out.print(z[2]+" "+z[0]+" "+z[1]);
+       }  
     }
 }
