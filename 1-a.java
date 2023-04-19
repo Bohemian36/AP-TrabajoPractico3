efd8243bb2f3508ca9899e38e1ff0c5d3e268312
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-04-19 11:15:57 -0300

    Update 1-a.java

--- 1-a.java
+++ 1-a.java
@@ -3,24 +3,16 @@
 public class 1-a {
     public static void main (String [] args) {
         
-      String z = "Bohemian Rapsody";
-
-      int contador = 0;
-      int lugar = 0;
-      char caracter = 'a';
-
-      lugar = z.indexOf(caracter);
-
+       String z = "Bohemian Rapsody";
+       int contador = 0;
+       int lugar = 0;
+       char caracter = 'a';
+          lugar = z.indexOf(caracter);
 
       while (lugar != -1 ) {
-      contador++;
-      lugar = z.indexOf(caracter,lugar+1);
+         contador++;
+         lugar = z.indexOf(caracter,lugar+1);
      }
-
-    System.out.println("En el título \"Bohemian Rapsody\", la letra 'a' aparece " + contador +" veces.");
-
+       System.out.println("En el título \"Bohemian Rapsody\", la letra 'a' aparece " + contador +" veces.");
  }
 }
-
-
-
