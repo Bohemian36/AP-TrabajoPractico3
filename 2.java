61d1ce9df8775f6fb1845a34d11cd91ce41e1ce0
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-04-30 21:28:37 -0300

    Update and rename 2-.java to 2.java

--- 2.java
+++ 2.java
@@ -1,14 +1,9 @@
-public class 2- {
+public class 2 {
     public static void main (String [] args) {
-        
        String z = "Bink 182;Twenty one pilots;Paramore;My chemical romance;Bob Marley;Charly Garcia;Nirvana;Queen;Lana del Rey;Pink Floyd;";
        String split [] = z.split(";");
-
-      for (String elemento : split) {
-      System.out.println(elemento);
-
-}
-}
-}
-
-
+         for (String elemento : split) {
+          System.out.println(elemento);
+       }
+     }
+   }
