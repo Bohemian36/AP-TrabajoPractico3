4101a7c01d61e977c27dca6da7e2c6061fb796c3
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-04-30 21:27:24 -0300

    Update and rename 1-c.java to 1c.java

--- 1c.java
+++ 1c.java
@@ -1,18 +1,13 @@
-//Ejercicio 1-c
-
-public class III {
+public class 1c {
     public static void main(String args[]) {
-     
       int [] z = new int [] {36,90,16};
       int num = 20;
       int suma = 0;
-      
-      for (int i=0;i<z.length;i++) {
+       for (int i=0;i<z.length;i++) {
            if (z[i] > num) {
             suma = suma + z[i];
-
-      System.out.print(z[i] + " ");
+             System.out.print(z[i] + " ");
+           }
+       }
     }
-}
-}
 }
