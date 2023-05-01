a76e3de5ce7bcd9bde8cc31999c67a6015e86319
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-04-30 21:03:28 -0300

    Update and rename 1-a.java to 1a.java

--- 1a.java
+++ 1a.java
@@ -1,18 +1,14 @@
-//Ejercicio 1(a)
-
-public class 1-a {
+public class 1a {
     public static void main (String [] args) {
-        
        String z = "Bohemian Rapsody";
        int contador = 0;
        int lugar = 0;
        char caracter = 'a';
-          lugar = z.indexOf(caracter);
-
-      while (lugar != -1 ) {
-         contador++;
-         lugar = z.indexOf(caracter,lugar+1);
+         lugar = z.indexOf(caracter);
+         while (lugar != -1 ) {
+          contador++;
+          lugar = z.indexOf(caracter,lugar+1);
      }
        System.out.println("En el título \"Bohemian Rapsody\", la letra 'a' aparece " + contador +" veces.");
+  }
  }
-}
